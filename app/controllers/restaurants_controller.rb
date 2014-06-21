5649dcf94123098e85f48fdb4b402fa70fa9cdde
Alek <agrinb@gmail.com>
2014-06-21 19:58:04 -0400

    added restaurant index and new view, added methods to restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,32 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+    def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      flash[:notice] = "Success!"
+      redirect_to '/restaurants'
+    else
+      flash.now[:notice] = "Your restaurant couldn't be saved."
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :city, :state, :zipcode)
+  end
 end
