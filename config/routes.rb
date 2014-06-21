cd0e482c9ab7fa0a326590c97799e5c3d0d4b7e2
Alek <agrinb@gmail.com>
2014-06-21 19:25:39 -0400

    drew routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'restaurants#index'
+  resources :restaurants, only: [:index, :new, :show, :create] do
+    resources :reviews, only: [:new, :show, :create]
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
