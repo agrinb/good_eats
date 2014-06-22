037781ee45265427b6a5cddac3751afc2f5c7f83
Alek <agrinb@gmail.com>
2014-06-21 20:35:48 -0400

    changed restaurant views, started working on reviews controller and pages

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      flash[:notice] = "Success!"
+      redirect_to '/reviews'
+    else
+      flash.now[:notice] = "Your review couldn't be saved."
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :body)
+  end
 end
